b13448caa9b7477a27fa504c03280aeb2bf928ee
yuta.onishi <rad6242@icloud.com>
2025-02-11 18:04:37 +0900

    taskモデルを生成する

---
+++ db/migrate/20250211090258_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :tasks do |t|
+      t.belongs_to :parent_task, null: true, foreign_key: { to_table: :tasks }
+      t.integer :status, null: false, default: 0
+      t.string :title, null: false
+      t.text :note
+      t.timestamps
+    end
+  end
+end
