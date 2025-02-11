48e8c2ac3f3b894877bb9e3b6734b2fe33a6858b
yuta.onishi <rad6242@icloud.com>
2025-02-11 18:08:11 +0900

    TaskモデルにEnumをvalidationを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  enum status: {
+    todo: 0,
+    doing: 1,
+    pending: 2,
+    done: 3,
+    archived: 4
+  }
+
+  validates :title, presence: true
+  validates :status, presence: true
 end
