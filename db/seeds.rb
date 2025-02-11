72b905702b304cc626bc579046a308ba0fd2e46d
yuta.onishi <rad6242@icloud.com>
2025-02-11 18:37:09 +0900

    seedの量を増やす

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,9 @@
 
 Task.create(title: "Root Task", status: :todo)
 Task.create(title: "Sub Task 1", status: :doing, parent_task: Task.first)
+Task.create(title: "Sub Task 1-1", status: :pending, parent_task: Task.first.sub_tasks.first)
+Task.create(title: "Sub Task 1-2", status: :done, parent_task: Task.first.sub_tasks.first)
+Task.create(title: "Sub Task 1-3", status: :archived, parent_task: Task.first.sub_tasks.first)
 Task.create(title: "Sub Task 2", status: :pending, parent_task: Task.first)
+Task.create(title: "Sub Task 2-1", status: :done, parent_task: Task.first.sub_tasks.last)
+Task.create(title: "Sub Task 2-2", status: :archived, parent_task: Task.first.sub_tasks.last)
