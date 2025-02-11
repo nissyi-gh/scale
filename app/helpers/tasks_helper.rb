e8432daed98b52c4a143904c58e10a665e20ec8b
yuta.onishi <rad6242@icloud.com>
2025-02-11 21:34:24 +0900

    taskのステータスを表示できるようにする

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -5,10 +5,20 @@ module TasksHelper
         task.sub_tasks.each do |sub_task|
           concat(content_tag(:li) do
             concat sub_task.title
+            concat render_task_status(sub_task)
             concat render_sub_task_tree(sub_task)
           end)
         end
       end
+    end
+  end
+
+  def render_task_status(task)
+    case task.status
+    when "done"
+      content_tag(:span, "✓", class: "status")
+    else
+      content_tag(:span, task.status, class: "status")
     end
   end
 end
