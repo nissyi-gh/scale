120634ae6466eeb04338a40a181c23c7f2ed1670
yuta.onishi <rad6242@icloud.com>
2025-02-11 22:02:36 +0900

    タスク追加機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,17 @@
 class TasksController < ApplicationController
   def index
+    @task = Task.new
     @tasks = Task.all
+  end
+
+  def create
+    @task = Task.create!(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :parent_task_id)
   end
 end
