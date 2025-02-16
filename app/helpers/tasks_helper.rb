846812d3decaab41158cb1271be3c67b99000f37
yuta.onishi <rad6242@icloud.com>
2025-02-16 19:07:23 +0900

    levelごとにスタイルを変更する

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,14 +1,14 @@
 module TasksHelper
-  def render_sub_task_tree(task)
+  def render_sub_task_tree(task, level = 0)
     if task.sub_tasks.any?
       content_tag(:ol) do
         task.sub_tasks.each do |sub_task|
-          concat(content_tag(:li) do
+          concat(content_tag(:li, class: "level-#{level}") do
             concat content_tag(:span, "#{sub_task.id}: ", class: "task-id")
             concat sub_task.title
             concat render_task_status(sub_task)
             concat render_task_controls(sub_task)
-            concat render_sub_task_tree(sub_task)
+            concat render_sub_task_tree(sub_task, level + 1)
           end)
         end
       end
